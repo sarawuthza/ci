8430c98ad9e07c95e26717c51be4a720de9d68c2
sarawuthza <sarawuthna@hotmail.com>
2016-11-26 16:20:56 +0700

    add footer

--- application/views/layout/footer_view.php
+++ application/views/layout/footer_view.php
@@ -1,3 +1,9 @@
+<div class="footer">
+    <div class="inner">
+        <p>Cover template for <a href="https://getbootstrap.com">Bootstrap demo</a>, by <a href="https://twitter.com/mdo">@Terk</a>.</p>
+    </div>
+</div>
+
 <script src="<?php echo base_url('bower_components/jquery/dist/jquery.min.js')?>"></script>
 <script src="<?php echo base_url('bower_components/bootstrap/dist/js/bootstrap.min.js')?>"></script>
 </body>
--- application/views/layout/header_view.php
+++ application/views/layout/header_view.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <link rel="stylesheet" href="<?php echo base_url('bower_components/bootstrap/style.css')?>">
     <link rel="stylesheet" href="<?php echo base_url('bower_components/bootstrap/dist/css/bootstrap.min.css')?>">
     <link rel="stylesheet" href="<?php echo base_url('bower_components/font-awesome/css/font-awesome.min.css')?>">
     <link rel="stylesheet" href="<?php echo base_url('bower_components/animate.css/animate.min.css')?>">
