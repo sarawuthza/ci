a6fac85807a660b288b348ba528f92f74ced3552
sarawuthza <sarawuthna@hotmail.com>
2016-11-26 14:26:15 +0700

    chang .htaccess

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,7 +6,20 @@
  * Date: 26/11/2559
  * Time: 13:25
  */
-class Home
+class Home extends CI_Controller
 {
+    public function index() {
+        echo "Hello soda";
+    }
+
+    public function x($name = "DJ soda", $lastname = "eiei")
+    {
+        echo "My name ".$name ."  ".$lastname;
+    }
+
+    public function calculator($x = 2, $y = 4)
+    {
+        echo $x + $y;
+    }
 
 }
