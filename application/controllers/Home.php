8fd89f70527edc58a8e9edc1c2e0921eeef3bc29
sarawuthza <sarawuthna@hotmail.com>
2016-11-26 16:13:11 +0700

    add test and footer_view

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,7 +9,15 @@
 class Home extends CI_Controller
 {
     public function index() {
+        $this->load->view('layout/header_view');
         $this->load->view('home_view');
+        $this->load->view('layout/footer_view');
     }
 
+    public function test()
+    {
+        $this->load->view('layout/header_view');
+        $this->load->view('test_view');
+        $this->load->view('layout/footer_view');
+    }
 }
---
+++ application/views/layout/header_view.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="<?php echo base_url('bower_components/bootstrap/dist/css/bootstrap.min.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('bower_components/font-awesome/css/font-awesome.min.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('bower_components/animate.css/animate.min.css')?>">
+    <title>Document</title>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse navbar">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Start Bootstrap</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
