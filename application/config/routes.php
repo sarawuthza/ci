19b0f5c4556c3921db64911c59625e3ff8cde392
sarawuthza <sarawuthna@hotmail.com>
2016-11-27 13:13:36 +0700

    add admin_view

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,5 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 $route['default_controller'] = 'home';
+$route['admin'] = 'admin/admin';
+$route['admin/(:any)'] = 'admin/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -10,6 +10,9 @@ class Admin extends CI_Controller
 {
     public function index()
     {
+        $this->load->view('admin/layout/admin_header_view');
+        $this->load->view('admin/layout/admin_sidebar_view');
         $this->load->view('admin/admin_view');
+        $this->load->view('admin/layout/admin_footer_view');
     }
 }
